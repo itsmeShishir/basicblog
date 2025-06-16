e66e8f437149eeae24dcfaf131c23e038121fcd6
itsmeShishir <91515501+itsmeShishir@users.noreply.github.com>
2025-06-16 12:05:12 +0545

    just basic one

--- 03_Django-blog/blog/urls.py
+++ 03_Django-blog/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, blog, about, contact
+from .views import home, blog, about, contact, blog_detail
 urlpatterns = [
     path("", home, name="home"),
     path("blog", blog, name="blog"),
+    path("post/<slug>", blog_detail, name="post"),
     path("about", about, name="about"),
     path("contact", contact, name="contact"),
 ]
--- 03_Django-blog/blog/views.py
+++ 03_Django-blog/blog/views.py
@@ -7,15 +7,31 @@ from categorys.models import Category
 
 # Create your views here.
 def home(request):
-    isSlider = Blog.objects.filter(isSlider=True)
-    isFeatured = Blog.objects.filter(isFeatured=True)
-    category = Category.objects.all()
+    isSlider = Blog.objects.filter(isSlider=True).order_by('-created_at')
+    isFeatured = Blog.objects.filter(isFeatured=True).order_by('-created_at')
+    isFeaturs = isFeatured[:1]
+    category = Category.objects.filter(isShow=True).order_by('-id')
+    fourCategory = category[:4]
+    blog = Blog.objects.all().order_by('-created_at')
+    fourBlog= blog[:4]
     context = {
         "isSlider": isSlider,
-        "isFeatured": isFeatured,
-        "category": category
+        "isFeaturs": isFeaturs,
+        "fourCategory": fourCategory, 
+        "fourBlog": fourBlog
     }
     return render(request, "index.html", context)
+
+def blog_detail(request, slug): 
+    post = Blog.objects.get(slug=slug)
+    print(post)
+    related = Blog.objects.filter(category=post.category).exclude(slug=slug)
+    context = {
+        "post": post,
+        "related": related
+    }
+    return render(request, "post.html", {"context": context})
+
 
 def blog(request):
     allblog = Blog.objects.all()
